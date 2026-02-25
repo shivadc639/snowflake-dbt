e34856ed5adb467972ffae912fae4c66fecbc0d3
shivadc <151916502+shivadc639@users.noreply.github.com>
2026-02-25 18:21:30 +0530

    Delete models directory

--- models/bronze/bronze_inventory.sql
+++
@@ -1,1 +0,0 @@
-select * from {{ source ('TPCDS_SF100TCL', 'INVENTORY') }}
--- models/silver/inventory_silver.sql
+++
@@ -1,2 +0,0 @@
-select inv_item_sk, inv_warehouse_sk, count(*) cnt   
-from {{ref('bronze_inventory')}} group by inv_item_sk, inv_warehouse_sk
